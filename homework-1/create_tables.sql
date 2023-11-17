8f9dbda7edcd7759622fb8239deb2423c6d6ad18
tatyana lozovaya <tatka8632@gmail.com>
2023-11-17 12:21:01 +0300

    homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) not null,
+	contact_name varchar(100) not null
+);
+
+CREATE TABLE employees(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	title varchar(100) not null,
+	birth_date date not null,
+	notes text not null
+);
+
+CREATE TABLE orders(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date not null,
+	ship_city varchar(100) not null
+);
+
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,30 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+# подключаемся с бд
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1111')
+# создаем курсор
+cur = conn.cursor()
+
+# считываем данные из csv и переносим в бд
+with open('north_data/customers_data.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        cur.execute('INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)',
+                    (row['customer_id'], row['company_name'], row['contact_name']))
+
+with open('north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        cur.execute("INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (row['employee_id'], row['first_name'], row['last_name'], row['title'], row['birth_date'], row['notes']))
+
+with open('north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        cur.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                    (row['order_id'], row['customer_id'], row['employee_id'], row['order_date'], row['ship_city'],))
+
+conn.commit()
+conn.close()
